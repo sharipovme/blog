2a5b1ff35d8241208a4456b6992d32af87538d4c
sharipovme <sharipovsdev@gmail.com>
2024-05-31 16:22:02 +0500

    added logout button

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,19 +1,30 @@
 import { BlogList } from "@/components/blog-list";
 import Can from "@/components/can";
 import { Button } from "@/components/form/button";
+import { useAuth } from "@/store/auth";
 import { useNavigate } from "react-router-dom";
 
 const Blog = () => {
+  const { setAuth, setRole } = useAuth();
   const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    setAuth(false);
+    setRole("");
+  };
 
   return (
     <Can i="blog.read" redirect>
       <div className="container section">
         <div className="flex">
           <h2>Latest Posts</h2>
-          <Can i="blog.create">
-            <Button onClick={() => navigate("/blog/create")}>Create</Button>
-          </Can>
+          <div className="right-buttons">
+            <Can i="blog.create">
+              <Button onClick={() => navigate("/blog/create")}>Create</Button>
+            </Can>
+            <Button onClick={logout}>Log out</Button>
+          </div>
         </div>
         <BlogList />
       </div>
